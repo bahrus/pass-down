9b9c8aaa56369c4efde8862826ee4b865c396d57
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-15 11:55:50 -0400

    ...

--- p-d.js
+++ p-d.js
@@ -1,6 +1,6 @@
 import { PassDown } from './pass-down.js';
-import { define } from 'xtal-element/lib/define.js';
+import { def } from 'trans-render/lib/def.js';
 export class PD extends PassDown {
     static is = 'p-d';
 }
-define(PD);
+def(PD);
