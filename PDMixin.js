8580dee0bc7eb06bc31a59c7b068b1f82741fcea
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-15 13:31:09 -0400

    ...

--- PDMixin.js
+++ PDMixin.js
@@ -5,9 +5,8 @@ import { passValToMatches, passVal, getProp } from 'on-to-me/on-to-me.js';
 const p_std = 'p_std';
 export const PDMixin = (superclass) => class extends superclass {
     addDefaultMutObs(self) {
-        var _a;
         const { lastVal, to, careOf, prop, as } = self;
-        const parent = (_a = getFrom(self)) === null || _a === void 0 ? void 0 : _a.parentElement;
+        const parent = getFrom(self)?.parentElement;
         if (parent) {
             const mutObs = document.createElement('mut-obs');
             const s = mutObs.setAttribute.bind(mutObs);
@@ -73,8 +72,7 @@ export function isMatchAfterFrom(match, self) {
     return false;
 }
 export function addDefaultMutObs(self) {
-    var _a;
-    const parent = (_a = getFrom(self)) === null || _a === void 0 ? void 0 : _a.parentElement;
+    const parent = getFrom(self)?.parentElement;
     if (parent) {
         const mutObs = document.createElement('mut-obs');
         const s = mutObs.setAttribute.bind(mutObs);
--- p-d.js
+++ p-d.js
@@ -1,6 +1,6 @@
 import { PassDown } from './pass-down.js';
 import { def } from 'trans-render/lib/def.js';
 export class PD extends PassDown {
+    static is = 'p-d';
 }
-PD.is = 'p-d';
 def(PD);
